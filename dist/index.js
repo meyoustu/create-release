22a31fb9f8eb12507fadf7c4dd3c09b295d83ffc
Liangcheng Juves <liangchengj@outlook.com>
2021-04-08 10:02:15 +0800

    Enable workflow to get TAG

--- dist/index.js
+++ dist/index.js
@@ -8837,6 +8837,7 @@ async function run() {
     core.setOutput('id', releaseId);
     core.setOutput('html_url', htmlUrl);
     core.setOutput('upload_url', uploadUrl);
+    core.setOutput('tag', tag);
   } catch (error) {
     core.setFailed(error.message);
   }
